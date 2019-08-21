5f8d02ef6c9498e0613260a4634f5301419256fe
CryptoSalamander <dhy03196@naver.com>
2019-08-21 15:10:50 +0900

    [MOD]Traning Save Method Update

--- machinelearning/Web Server/train.py
+++ machinelearning/Web Server/train.py
@@ -1,11 +1,13 @@
 import tensorflow as tf
 import pandas as pd
+import sys
 from sklearn import preprocessing
+
 tf.set_random_seed(777)  # for reproducibility
 
-train1_csv = pd.read_csv("/nfs/data/negative_train.csv")
+train1_csv = pd.read_csv("../data/negative_train.csv")
 temp1_csv = train1_csv
-train2_csv = pd.read_csv("/nfs/data/positive_train.csv")
+train2_csv = pd.read_csv("../data/positive_train.csv")
 temp2_csv = train2_csv
 
 train_csv = pd.concat([train1_csv,train2_csv], sort=False)
@@ -23,14 +25,14 @@ x_data = train_std
 y_data = train_csv[["label"]]
 
 # placeholders for a tensor that will be always fed.
-X = tf.placeholder(tf.float32, shape=[None, 5])
-Y = tf.placeholder(tf.float32, shape=[None, 1])
+X = tf.placeholder(tf.float32, shape=[None, 5],name="X")
+Y = tf.placeholder(tf.float32, shape=[None, 1],name="Y")
 
-W = tf.Variable(tf.random_normal([5, 1]), name='weight')
-b = tf.Variable(tf.random_normal([1]), name='bias')
+W = tf.Variable(tf.random_normal([5, 1]), name='W')
+b = tf.Variable(tf.random_normal([1]), name='b')
 
 # Hypothesis using sigmoid: tf.div(1., 1. + tf.exp(tf.matmul(X, W)))
-hypothesis = tf.sigmoid(tf.matmul(X, W) + b)
+hypothesis = tf.sigmoid(tf.matmul(X, W) + b, name="h")
 
 # cost/loss function
 cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) *
@@ -48,17 +50,27 @@ with tf.Session() as sess:
     # Initialize TensorFlow variables
     sess.run(tf.global_variables_initializer())
 
-    for step in range(10001):
+    for step in range(30001):
         cost_val, _ = sess.run([cost, train], feed_dict={X: x_data, Y: y_data})
-        if step % 500 == 0:
+        if step % 1000 == 0:
             print(step, cost_val)
 
     # Accuracy report
     h, c, a = sess.run([hypothesis, predicted, accuracy],
                        feed_dict={X: x_data, Y: y_data})
     print("\nHypothesis: ", h, "\nCorrect (Y): ", c, "\nAccuracy: ", a)
-    saver = tf.train.Saver()
-    save_path = saver.save(sess, "/nfs/model/saved.cpkt")
-    builder = tf.saved_model.builder.SavedModelBuilder("/nfs/model/trained_model")
-    builder.add_meta_graph_and_variables(sess,["serve"])
-    builder.save()
+    print("\nWeight : \n",sess.run(W))
+    final_weight = sess.run(W)
+    final_bias = sess.run(b)
+    filepath = "./model/server.txt"
+    file = open(filepath, 'w', encoding='utf8')
+    for a in range(0,5):
+        line = str(final_weight[a])[1:-1] + "\n"
+        file.write(line)
+    file.write(str(final_bias)[1:-1])
+    file.close()
+
+
+    print("\nBias : \n",sess.run(b))
+    saver = tf.train.Saver([W,b])
+    save_path = saver.save(sess, "./model/saved.cpkt")
